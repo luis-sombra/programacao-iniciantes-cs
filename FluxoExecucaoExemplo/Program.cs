8a6c25f6ad7f5700e5923909516346a8a1e19e18
Luis Sombra <contato@luissombra.com>
2018-11-22 12:15:10 -0200

    Adiciona o projeto que demonstra o fluxo de execução

---
+++ FluxoExecucaoExemplo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FluxoExecucaoExemplo
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("verde");
+            Console.WriteLine("amarelo");
+            Console.WriteLine("azul");
+            Console.WriteLine("laranja");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
