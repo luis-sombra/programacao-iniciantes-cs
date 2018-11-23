9dd1391db9a015ed9db0188f834331296909efc0
Luis Sombra <contato@luissombra.com>
2018-11-23 18:38:15 -0200

    fluxo de execução - exercício 1
    adiciona o projeto do exercício: fluxo de execução #1

---
+++ FluxoExecucaoExercicio1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FluxoExecucaoExercicio1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("abacaxi");
+            Console.WriteLine("uva");
+            MessageBox.Show("melancia");
+            Console.WriteLine("banana");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
